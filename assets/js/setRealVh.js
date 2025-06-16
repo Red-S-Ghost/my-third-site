c75fcab1bd7d49276b434b8d5f60c4dcbc4e446a
Oleksandr <48192282+Red-S-Ghost@users.noreply.github.com>
2025-06-16 15:21:07 +0300

    Delete assets directory

--- assets/js/setRealVh.js
+++
@@ -1,8 +0,0 @@
-
-function setRealVh() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-window.addEventListener('resize', setRealVh);
-window.addEventListener('orientationchange', setRealVh);
-setRealVh();
